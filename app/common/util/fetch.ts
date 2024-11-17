162a22de74e4ae247e37799aa9678f904d366879
Dima <dziurziadmytro@gmail.com>
2024-11-17 21:50:46 +0200

    add ws for products

--- app/common/util/fetch.ts
+++ app/common/util/fetch.ts
@@ -2,19 +2,15 @@ import { cookies } from 'next/headers';
 import { API_URL } from '../constants/api';
 import { getErrorMessage } from './errors';
 
-export const getHeaders = async () => {
-  const cookieHeader = await cookies();
-  return {
-    Cookie: cookieHeader.toString(),
-  };
-};
+export const getHeaders = () => ({
+  Cookie: cookies().toString(),
+});
 
 export const post = async (path: string, data: FormData | object) => {
   const body = data instanceof FormData ? Object.fromEntries(data) : data;
-  const headers = await getHeaders();
   const res = await fetch(`${API_URL}/${path}`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', ...headers },
+    headers: { 'Content-Type': 'application/json', ...getHeaders() },
     body: JSON.stringify(body),
   });
   const parsedRes = await res.json();
@@ -24,10 +20,10 @@ export const post = async (path: string, data: FormData | object) => {
   return { error: '', data: parsedRes };
 };
 
-export const get = async <T>(path: string, tags?: string[]) => {
-  const headers = await getHeaders();
-  const res = await fetch(`${API_URL}/${path}`, {
-    headers: { ...headers },
+export const get = async <T>(path: string, tags?: string[], params?: URLSearchParams) => {
+  const url = params ? `${API_URL}/${path}?` + params : `${API_URL}/${path}`;
+  const res = await fetch(url, {
+    headers: { ...getHeaders() },
     next: { tags },
   });
   return res.json() as T;
